1e02d52d60aed7d264bd44ef3236e7774364747b
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2023-12-17 21:12:01 -0500

    Begin exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
+
+data = []
+
+f = open("Data/portfolio.csv", "rt")
+headers = next(f).split(",")
+
+for line in f:
+    row = line.split(",")
+    data.append(row)
+
+f.close()
+
+#print(data)
+
+total = 0
+
+for rowdata in data:
+    subtotal = float(rowdata[1]) * float(rowdata[2])
+    total = total + subtotal
+    
+print(f"The total is: ${total}")
